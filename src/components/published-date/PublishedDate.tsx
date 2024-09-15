1924ac21a706b6a9641f8563e219292676ccecb0
skyv26 <dev.aakashv@gmail.com>
2024-09-15 16:51:09 +0530

    update: add the missing text

--- src/components/published-date/PublishedDate.tsx
+++ src/components/published-date/PublishedDate.tsx
@@ -4,7 +4,7 @@ const PublishedDate: React.FC = () => {
   const { content } = useMyContext();
   return (
     <p className="text-sm text-[hsl(0,0%,7%)] tracking-tight font-md">
-      {content.date}
+      Published {content.date}
     </p>
   );
 };
